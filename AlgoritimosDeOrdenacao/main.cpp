1c22a65db6d33570592e77ba460dfa80134cc45c
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2022-10-12 23:05:39 -0300

    Started working on the menu

--- AlgoritimosDeOrdenacao/main.cpp
+++ AlgoritimosDeOrdenacao/main.cpp
@@ -4,13 +4,20 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui-SFML.h"
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define MENU_WIDTH 400
+
+bool isVisual = false;
+int vectorSize = 1000;
+int sps = 0;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Algoritioms de ordenação");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Algoritioms de ordenação", sf::Style::Titlebar | sf::Style::Close);
 
     ImGui::SFML::Init(window);
-
+    
 
     sf::Event e;
 
@@ -27,9 +34,42 @@ int main()
             }
         }
         ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::SetNextWindowSize(ImVec2(MENU_WIDTH, WINDOW_HEIGHT));
+        ImGui::SetNextWindowPos(ImVec2(WINDOW_WIDTH - (MENU_WIDTH), 0));
+        ImGui::Begin(u8"CONFIGURAÇÃO", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+        ImGui::SetNextItemOpen(true);
 
-        ImGui::Begin("Janela");
-        ImGui::Text("Sou uma janela do ImGui");
+        ImGui::Checkbox("Visual ", &isVisual);
+        if (ImGui::IsItemHovered())
+            ImGui::SetTooltip(u8"- Uma representação visual do algoritimo\n\n- Para uma medição da performance real por favor desabilite");
+        /*
+        if (ImGui::TreeNode("Representacao visual"))
+        {
+            ImGui::Spacing();
+            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 255, 255, 120));
+            ImGui::TextWrapped("Uma representacao visual do algoritimo, para uma medicao da performance real por favor desabilite\n\n");
+            ImGui::PopStyleColor();
+            ImGui::Checkbox("Visual ", &isVisual);
+            if (ImGui::IsItemHovered())
+                ImGui::SetTooltip(u8"Uma representação visual do algoritimo\nPara uma medição da performance real por favor desabilite");
+            ImGui::TreePop();
+        }
+        */
+        ImGui::Spacing();
+        ImGui::PushItemWidth(150);
+        ImGui::InputInt("Tamanho do vetor", &vectorSize);
+        ImGui::PopItemWidth();
+        if (ImGui::IsItemHovered())
+            ImGui::SetTooltip(u8"- Define o tamanho do vetor a ser ordenado");
+
+        ImGui::Spacing();
+        ImGui::PushItemWidth(150);
+        ImGui::InputInt("PPS", &sps);
+        ImGui::PopItemWidth();
+        if (ImGui::IsItemHovered())
+            ImGui::SetTooltip(u8"- Define a quantidade de PASSOS POR SEGUNDO\n\n- Coloque \" 0 \" para definir como infinito\n\n- Apenas aplicavel quando \"Visual\" está ativado");
+
+       
         ImGui::End();
 
 
